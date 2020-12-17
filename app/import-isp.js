fcf192497714d637c806b189a0c8342b903267d0
Mauro Giannandrea <giannandrea@Giannandrea-MACBOOKPRO.local>
2020-12-17 10:20:25 +0100

    improve isp data

--- app/import-isp.js
+++ app/import-isp.js
@@ -7,7 +7,7 @@ fs.createReadStream('IP2LOCATION-LITE-ASN.CSV')
   .pipe(csv(['ip_form', 'ip_to', "cidr", "four", "isp"]))
   .on('data', (row) => {
           if (row.isp && row.isp.length > 2 && row.isp != "null") {
-          db.get("UPDATE ip2location_db SET isp = \""+row.isp+"\" where ip_from = "+row.ip_form+" and ip_to = "+row.ip_to+";", function(err, row) {
+          db.get("UPDATE ip2location_db SET isp = \""+row.isp+"\" where ip_from < "+row.ip_form+" and ip_to > "+row.ip_to+";", function(err, row) {
              if (err) { console.log(err); }
            });
            //console.log(row.isp);
